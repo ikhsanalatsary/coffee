14fcf7bffd3f39260ce4a031425d744c361291b0
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-08-18 19:48:11 +0700

    finish app

--- coffee/ViewController.h
+++ coffee/ViewController.h
@@ -10,6 +10,12 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *waterTextField;
+@property (weak, nonatomic) IBOutlet UITextField *ratioTextField;
+@property (weak, nonatomic) IBOutlet UITextField *coffeTextField;
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
+
+- (IBAction)calculateButtonPressed:(id)sender;
 
 @end
 
--- coffee/ViewController.m
+++ coffee/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _coffeTextField.enabled = NO;
+    [_calculateButton setTitle: _calculateButton.titleLabel.text.uppercaseString forState: UIControlStateNormal];
 }
 
 
@@ -25,5 +27,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)calculateButtonPressed:(id)sender {
+    
+    float water = [[self.waterTextField text] floatValue];
+    float ratio = [[self.ratioTextField text] floatValue];
+    
+//    NSLog(@"Water: %f, Ratio: % f", water, ratio);
+    
+    if (water && ratio) {
+        float coffee = water / ratio;
+        NSString *coffeText = [NSString stringWithFormat: @"%f", coffee];
+        self.coffeTextField.text = coffeText;
+    }
+    
+}
 
 @end
